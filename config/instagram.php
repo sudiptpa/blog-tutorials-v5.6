d792ca0f2a097e88bf6aa3a5a642c30b8ef16519
Sujip Thapa <sudiptpa@gmail.com>
2018-03-24 11:38:33 +0545

    Added demo instagram access token

--- config/instagram.php
+++ config/instagram.php
@@ -3,7 +3,6 @@
 return [
 
     'api' => [
-        'demo-access-token' => 'xxxxxxxx.xxxxxxx.xxxxxxxxx',
-        'access-token' => '7259672563.1677ed0.9800705994d14062b376e9defa3491ef',
+        'access-token' => 'xxxxx.xxx.xxxx', //replace xxx with your actual access token
     ],
 ];
